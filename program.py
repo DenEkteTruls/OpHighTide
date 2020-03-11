2949a702d99924c31b646675f32f7680244aa5ba
DenEkteTruls <44004719+DenEkteTruls@users.noreply.github.com>
2020-03-11 09:57:02 +0100

    Update program.py

--- program.py
+++ program.py
@@ -1,2 +1,22 @@
 import os
 import sys
+import cv2
+imoprt socket
+import threading
+import numpy as np
+
+import general
+
+camera = cv2.VideoCapture(int(sys.argv[1]))
+
+while True:
+  
+  frame = camera.read()[1]
+  
+  cv2.imshow("mainframe", frame)
+  
+  if general.checkQuit():
+    break
+    
+camera.release()
+cv2.destroyAllWindows()
