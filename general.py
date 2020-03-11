7f5854c261ff1d28a9bc125d796eb7bfc7ae81d6
DenEkteTruls <44004719+DenEkteTruls@users.noreply.github.com>
2020-03-11 10:00:55 +0100

    Update general.py

--- general.py
+++ general.py
@@ -1,1 +1,13 @@
+import os
+import sys
+import cv2
+import socket
+impor threading
+import numpy as np
 
+def checkQuit():
+  
+  if cv2.waitKey(20) & 0xFF == 27:
+    return True
+  else:
+    return False
